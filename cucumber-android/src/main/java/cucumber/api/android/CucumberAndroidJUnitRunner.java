e35439fc01bb084ebcbdef5647f0ad5004c188a0
Roman Havran <r.havran.dev@gmail.com>
2020-01-31 00:26:25 -0500

    Introduce a flag to use default AndroidJunitRunner
    This is done to add the possibility to run instrumentation UI tests (non-cucumber) with AndroidJUnitRunner without rebuilding tests with different runners based on the Gradle parameter.

--- cucumber-android/src/main/java/cucumber/api/android/CucumberAndroidJUnitRunner.java
+++ cucumber-android/src/main/java/cucumber/api/android/CucumberAndroidJUnitRunner.java
@@ -1,39 +1,28 @@
 package cucumber.api.android;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.test.runner.AndroidJUnitRunner;
-import cucumber.runtime.android.CucumberJUnitRunnerBuilder;
+
+import cucumber.runtime.android.CucumberAndroidJUnitArguments;
 
 /**
  * {@link AndroidJUnitRunner} for cucumber tests. It supports running tests from Android Tests Orchestrator
  */
-public class CucumberAndroidJUnitRunner extends AndroidJUnitRunner {
+public class CucumberAndroidJUnitRunner extends AndroidJUnitRunner implements CucumberArgumentsProvider {
 
-    public static final String CUCUMBER_ANDROID_TEST_CLASS = "cucumberAndroidTestClass";
-    private static final String ARGUMENT_ORCHESTRATOR_RUNNER_BUILDER = "runnerBuilder";
-    private static final String ARGUMENT_ORCHESTRATOR_CLASS = "class";
-    private Bundle arguments;
+    private CucumberAndroidJUnitArguments cucumberJUnitRunnerCore;
 
     @Override
     public void onCreate(final Bundle bundle) {
-        bundle.putString(ARGUMENT_ORCHESTRATOR_RUNNER_BUILDER, CucumberJUnitRunnerBuilder.class.getName());
-
-        String testClass = bundle.getString(ARGUMENT_ORCHESTRATOR_CLASS);
-        if (testClass != null && !testClass.isEmpty()){
-            //if this runner is executed for single class (e.g. from orchestrator or spoon), we set
-            //special option to let CucumberJUnitRunner handle this
-             bundle.putString(CUCUMBER_ANDROID_TEST_CLASS, testClass);
-        }
-        //there is no need to scan all classes - we can fake this execution to be for single class
-        //because we delegate test execution to CucumberJUnitRunner
-        bundle.putString(ARGUMENT_ORCHESTRATOR_CLASS, CucumberJUnitRunnerBuilder.class.getName());
-        
-        arguments = bundle;
-
-        super.onCreate(bundle);
+        cucumberJUnitRunnerCore = new CucumberAndroidJUnitArguments(bundle);
+        super.onCreate(cucumberJUnitRunnerCore.processArgs());
     }
 
-    public Bundle getArguments() {
-        return arguments;
+    @NonNull
+    @Override
+    public CucumberAndroidJUnitArguments getArguments() {
+        return cucumberJUnitRunnerCore;
     }
 }
--- cukeulator/src/androidTest/java/cucumber/cukeulator/test/CukeulatorAndroidJUnitRunner.java
+++ cukeulator/src/androidTest/java/cucumber/cukeulator/test/CukeulatorAndroidJUnitRunner.java
@@ -11,12 +11,11 @@ import cucumber.api.android.CucumberAndroidJUnitRunner;
  * The CucumberOptions annotation is mandatory for exactly one of the classes in the test project.
  * Only the first annotated class that is found will be used, others are ignored. If no class is
  * annotated, an exception is thrown. This annotation does not have to placed in runner class
-*/
+ */
 @CucumberOptions(
         features = "features"
 )
 public class CukeulatorAndroidJUnitRunner extends CucumberAndroidJUnitRunner {
-
 
     @Override
     public void onCreate(final Bundle bundle) {
@@ -34,7 +33,7 @@ public class CukeulatorAndroidJUnitRunner extends CucumberAndroidJUnitRunner {
         String cucumber = "cucumber";
         String separator = "--";
         return "junit:" + getAbsoluteFilesPath() + "/" + cucumber + ".xml" + separator +
-                        "html:" + getAbsoluteFilesPath() + "/" + cucumber + ".html";
+                "html:" + getAbsoluteFilesPath() + "/" + cucumber + ".html";
     }
 
     /**
@@ -46,6 +45,6 @@ public class CukeulatorAndroidJUnitRunner extends CucumberAndroidJUnitRunner {
 
         //sdcard/Android/data/cucumber.cukeulator
         File directory = getTargetContext().getExternalFilesDir(null);
-        return new File(directory,"reports").getAbsolutePath();
+        return new File(directory, "reports").getAbsolutePath();
     }
 }
